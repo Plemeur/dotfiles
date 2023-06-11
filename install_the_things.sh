3a3a971c1db4a3327b11373b145574383b7b2df1
Pierre <pierre@pop-os.localdomain>
2023-06-11 09:14:12 +0900

    feat: packer

--- install_the_things.sh
+++ install_the_things.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 ## Install neovim
 echo "Installing Neovim"
 sudo apt install -y ninja-build gettext cmake unzip curl git
@@ -7,15 +9,25 @@ git checkout stable
 make -j8 CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+cd - 
+cp -r nvim ~/.config/nvim
+
+nvim --headless -c "PackerSync"
+
+
 ## Some terminal stuff
 echo "Installing Zsh"
 sudo apt install -y alacritty zsh tmux
-sudo chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh $USER
 
 ## Install some rust stuff
 sudo apt install -y cargo
 cargo install bat
 cargo install ripgrep
 cargo install fd-find
+
 
 
